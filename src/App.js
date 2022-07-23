dc59b2ca413d9b4dc98936aecb3731c127d3377a
spbavarva <84507631+spbavarva@users.noreply.github.com>
2022-07-23 09:15:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Hotel from "./pages/Hotel/Hotel";
+import List from "./pages/List/List";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured></Featured>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
