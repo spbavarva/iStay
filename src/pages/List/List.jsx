856165c35635dabeb39d1f4599e8f82f4ab14ac2
spbavarva <84507631+spbavarva@users.noreply.github.com>
2022-07-24 12:58:53 +0530

    half search

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,12 +1,55 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+            </div>
+           
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                
+                
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
